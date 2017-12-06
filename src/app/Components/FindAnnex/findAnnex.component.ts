094525a3a4addc9be2b68c236a401e71a37c964a
Sidath Asiri <sidathasiri@gmail.com>
2017-12-06 17:59:52 +0530

    added find annex component

---
+++ src/app/Components/FindAnnex/findAnnex.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'find-annex',
+  templateUrl: './findAnnex.component.html',
+})
+export class FindAnnexComponent {
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/HomeComponent/home.component';
 import { NavComponent } from './Components/Shared/Navbar/navbar.component';
+import { FindAnnexComponent } from './Components/FindAnnex/findAnnex.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    FindAnnexComponent
   ],
   imports: [
     BrowserModule,
