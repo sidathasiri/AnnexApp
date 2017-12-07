013f677653ac6a719fafc25d450b481873c5cf2c
Sidath Asiri <sidathasiri@gmail.com>
2017-12-07 23:14:31 +0530

    added dynamically loading districts on province

--- src/app/Components/FindAnnex/findAnnex.component.ts
+++ src/app/Components/FindAnnex/findAnnex.component.ts
@@ -6,15 +6,28 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
   selector: 'find-annex',
   templateUrl: './findAnnex.component.html',
 })
-export class FindAnnexComponent implements OnInit{
+export class FindAnnexComponent implements OnInit {
 
   provinces: any[];
+  currentProvince: any = {};
 
-  constructor(private commonService: CommonService){}
+  constructor(private commonService: CommonService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.commonService.getProvinces()
-    .subscribe(res => this.provinces = res);
+      .subscribe(res => this.provinces = res);
+  }
+
+  setProvince(province: string) {
+    if (province == 'Any') {
+      this.currentProvince = {};
+    } else {
+      for (var i = 0; i < this.provinces.length; i++) {
+        if (this.provinces[i].name == province) {
+          this.currentProvince = this.provinces[i];
+        }
+      }
+    }
   }
 
 }
