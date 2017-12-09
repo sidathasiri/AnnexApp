9f26300bc7bce30e8a922b7355f95296c00619aa
Sidath Asiri <sidathasiri@gmail.com>
2017-12-09 08:21:35 +0530

    added ngModel to signup form

--- src/app/Components/Signup/signup.component.ts
+++ src/app/Components/Signup/signup.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'signup',
   templateUrl: './signup.component.html'
 })
 export class SignupComponent {
+  email: string;
+  password1: string;
+  password2: string;
+
+  signup(form:any){
+    console.log(form);
+    console.log(this.email+"  "+ this.password1+"  "+this.password2);
+  }
 }
